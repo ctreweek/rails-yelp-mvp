563383179f83d4c0832ae949af7972fb9735c925
Courtney Treweek <c.k.treweek@gmail.com>
2017-06-07 12:11:32 +0200

    added seeds and restaurants and reviews controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "14354567",
+    category:     "Chinese"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "123456",
+    category:     "Indian"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "45678542",
+    category:     "Italian"
+  },
+  {
+    name:         "McDonalds",
+    address:      "123 Street",
+    phone_number: "567568",
+    category:     "French"
+  },
+  {
+    name:         "Cosca",
+    address:      "456 Avenue",
+    phone_number: "4562425",
+    category:     "American"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
