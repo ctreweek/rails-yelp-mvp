7ec211f05ab759ab4cae59540317281452c850e3
Courtney Treweek <c.k.treweek@gmail.com>
2017-06-07 12:24:12 +0200

    added router

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,26 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.find(params[:id])
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new(restaurant_params)
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save!
+
+  end
+
+ private
+
+ def restaurant_params
+  params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+ end
+
 end
